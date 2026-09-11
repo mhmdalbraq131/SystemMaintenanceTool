1a83fd51246bd2120efbe81fd13d5fcbfe7b36b4
mhmdalbraq131 <mhmdalbraq131@gmail.com>
2026-09-12 02:43:28 +0300

    fix: make app package initializer valid Python

--- SystemMaintenanceTool/app/__init__.py
+++ SystemMaintenanceTool/app/__init__.py
@@ -1,5 +1,5 @@
-System Maintenance Tool Professional - Professional Windows System Management & Maintenance Suite
+"""System Maintenance Tool application package."""
 
-__version__ = '1.0.0'
-__app_name__ = 'System Maintenance Tool Professional'
-__author__ = 'SMT Professional'
+__version__ = "1.0.0"
+__app_name__ = "System Maintenance Tool Professional"
+__author__ = "SMT Professional"
